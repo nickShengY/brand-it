5b9cd43280cbbcf744c7dbbfb0a3396fddf3b9e8
Sheng Yun <syun13@fordham.edu>
2023-07-29 22:54:15 +0800

    Update results.tsx

--- brand-ui/components/results.tsx
+++ brand-ui/components/results.tsx
@@ -12,7 +12,7 @@ const Results: React.FC<ResultsProps> = (props) => {
     const element = (
       <div
         key={i}
-        className="bg-teal-200 p-1 text-teal-700 px-2 text-sm rounded-md"
+        className="bg-purple-200 p-1 text-teal-700 px-2 text-sm rounded-md"
       >
         #{props.keywords[i]}
       </div>
@@ -29,7 +29,7 @@ const Results: React.FC<ResultsProps> = (props) => {
 
   const resultSection = (label: string, body: any) => {
     return (
-      <div className="bg-slate-700 p-4 my-3 rounded-md">
+      <div className="bg-violet-700 p-4 my-3 rounded-md">
         <div className="text-slate-400 text-sm font-bold mb-4">{label}</div>
         <div>{body}</div>
       </div>
@@ -59,8 +59,8 @@ const Results: React.FC<ResultsProps> = (props) => {
 
       </div>
       <button
-        className="bg-gradient-to-r from-teal-400 
-        to-blue-500 disabled:opacity-50 w-full p-2 rounded-md text-lg"
+        className="bg-gradient-to-r from-yellow-200 
+        to-yello-500 disabled:opacity-75 w-full p-2 rounded-md text-lg"
         onClick={props.onBack}
       >
         Back
